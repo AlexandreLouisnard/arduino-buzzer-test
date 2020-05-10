162c103f4c45231dcbca3091436cf6d9aa810617
Alexandre Louisnard <alouisnard@gmail.com>
2020-05-10 22:55:20 +0200

    Press + and - to change buzzer freq, etc.

---
+++ src/main.h
@@ -0,0 +1,8 @@
+/**
+ * Plays a melody.
+ * 
+ * melody[]: the notes
+ * size: the melody[] size
+ * notesDuration[]: each note duration (1 (or 0, default) for a whole note, 4 for a quarter note, 8 for an eight note, etc.)
+ */
+void playMelody(int melody[], int size, int notesDurations[]);
